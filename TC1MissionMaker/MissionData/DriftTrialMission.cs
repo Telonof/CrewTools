10b9fc2e7502d2fcafc3f0aac182d68938299847
Telonof <115968757+Telonof@users.noreply.github.com>
2026-06-18 12:52:49 -0400

    [MissionMaker] Add scoring for drift trials

--- TC1MissionMaker/MissionData/DriftTrialMission.cs
+++ TC1MissionMaker/MissionData/DriftTrialMission.cs
@@ -20,10 +20,24 @@ namespace TC1MissionMaker.MissionData
             GenerateAtomElement("D975D698", "A273673B", BitConverter.GetBytes(id));
         }
 
+        public override bool ParseInstantStart(bool instant)
+        {
+            if (instant)
+                GenerateFullEntityElement("3D549BD4", "375B0000", "00C089C50000AF44", Convert.FromHexString("FFFFFFFFFFFFFFFF"));
+
+            return instant;
+        }
+
+        public override void ParseTTOG(string entity)
+        {
+            GenerateFullEntityElement("3D549BD4", "375B0000", "00C089C50000AF44", Convert.FromHexString(entity));
+        }
+
         public override void MissionSpecific()
         {
             GenerateSpawnpointEntity();
             GenerateRewardMovie(null);
+            ParseScoring();
 
             //timer
             float time = XMLUtil.GrabFloatOrDefault(_missionData, "time", 120, true);
@@ -38,17 +52,27 @@ namespace TC1MissionMaker.MissionData
             GenerateBoolElement("81EAE5AF", disableCarSelect);
         }
 
-        public override bool ParseInstantStart(bool instant)
+        private void ParseScoring()
         {
-            if (instant)
-                GenerateFullEntityElement("3D549BD4", "375B0000", "00C089C50000AF44", Convert.FromHexString("FFFFFFFFFFFFFFFF"));
+            XElement? scoreNode = _missionData.Element("scoring");
 
-            return instant;
-        }
+            if (scoreNode == null)
+                return;
 
-        public override void ParseTTOG(string entity)
-        {
-            GenerateFullEntityElement("3D549BD4", "375B0000", "00C089C50000AF44", Convert.FromHexString(entity));
+            //max multiplier
+            GenerateFullFloatElement("ADB80000", "000083C40000BD44", XMLUtil.GrabIntOrDefault(scoreNode, "maxMultiplier", 99, true));
+
+            //how many points are given each second
+            GenerateFullFloatElement("AE5C0000", "008003C50000B944", XMLUtil.GrabFloatOrDefault(scoreNode, "pointsPerSecond", 100, true));
+
+            //how long does it take until the next multiplier is given
+            GenerateFullFloatElement("ABB80000", "0000A3C40000BD44", XMLUtil.GrabFloatOrDefault(scoreNode, "multiplierWait", 0.8f, true));
+
+            //how long should the game wait until it banks the points.
+            GenerateFullFloatElement("D5B70000", "0000FBC40000C744", XMLUtil.GrabFloatOrDefault(scoreNode, "multiplierHoldTime", 1f, true));
+
+            //how much does the multiplier go up by? (Example: 50 means it goes from x1 to x51 instead of x2)
+            GenerateFullFloatElement("ACB80000", "000093C40000BD44", XMLUtil.GrabFloatOrDefault(scoreNode, "multiplierIncrease", 1f, true));
         }
     }
 }
